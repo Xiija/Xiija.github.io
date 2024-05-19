2e609493735ab48197f103ab92f15a05e9452aac
Xiija <36306194+Xiija@users.noreply.github.com>
2024-05-19 10:09:14 -0700

    Update index.js

--- playa/index.js
+++ playa/index.js
@@ -39,6 +39,7 @@ async function refresh() {
        let slurl = info.table.rows[0].c[0].v;     
        let tmp = slurl.replace("{people=","");
        tmp = tmp.replace("}","");
+       tmp = tmp.replaceAll(",", "<br>");
        // let slurl2 = JSON.parse(tmp);
        dynamicPart2.innerHTML = "ON PLAYA: <br><br>"  + tmp;  
       let currtime = time();
