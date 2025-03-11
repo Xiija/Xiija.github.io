f12cc13948e8813ec0744e6d88aab88f93d1f996
Xiija <36306194+Xiija@users.noreply.github.com>
2025-03-11 11:42:28 -0700

    Update index.js

--- Destiny2/index.js
+++ Destiny2/index.js
@@ -1,15 +1,16 @@
 let url       = "https://www.bungie.net/Platform/Settings";
 let dyn       = document.getElementById("dynamicPart");  
 async function refresh() {  
-    fetch(url)
-    .then(res  => res.json())
-    .then(data => {
-        let c = data.Response.systems.Destiny2;
-        let msg = c.enabled ? "The game is UP!" : "The game is Down!";       
-        dyn.innerText = "STATUS: " + msg;
-    })  
-    .catch(err => {   
-    });
+    // fetch(url)
+    // .then(res  => res.json())
+    // .then(data => {
+    //     let c = data.Response.systems.Destiny2;
+    //     let msg = c.enabled ? "The game is UP!" : "The game is Down!";       
+    //     dyn.innerText = "STATUS: " + msg;
+    // })  
+    // .catch(err => {   
+    // });
+     dyn.innerText = "STATUS: "
     setTimeout(refresh,30000);  // 30 sec
 };
 refresh();
