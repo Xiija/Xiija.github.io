4928428d20c9e308e53b0ff18096d95f07cb5b4d
Xiija <36306194+Xiija@users.noreply.github.com>
2025-03-13 18:15:36 -0700

    Update index.js

--- DiscordWebhook/index.js
+++ DiscordWebhook/index.js
@@ -1,19 +1,19 @@
 let disc      = "https://discord.com/api/webhooks/1149704323857657976/7jUG3nOop-1zIIs7GxKiQ_BxVyBPsrIzqX9CeUjYA29TaQYeTL_5rt78H7CMECt5UzXx";
-let disc2 = "";
+let disc2     = "https://spicy-ghost-94.webhook.cool";
 let txtbox    = document.getElementById("content");
 let chk       = document.getElementById("check");
 async function send() {  
      let  msg = txtbox.value;
-     chk.innerText = msg;
+     chk.innerText = "MSG: " + msg;
      txtbox.innerText = "";
 
     // ==========================
-    /*
+  
     let data = { 
       username: "Git-io D2 status",
       content: "Status: TEST\n" + msg
     };    
-    fetch(disc, {
+    fetch(disc2, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json' 
@@ -24,6 +24,6 @@ async function send() {
     .catch(error => {
       console.error('Error:', error); 
     });
-    */
+  
 // ==============================
 } // end send
