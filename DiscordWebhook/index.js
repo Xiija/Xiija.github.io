6337efcb03f820cdfc82eb8096e9002605f1e459
Xiija <36306194+Xiija@users.noreply.github.com>
2025-03-13 18:22:22 -0700

    Update index.js

--- DiscordWebhook/index.js
+++ DiscordWebhook/index.js
@@ -1,3 +1,5 @@
+// GitHub pages only support static websites/content, PHP doesn't work on GitHub pages and GitHub doesn't allow POST requests
+
 let disc      = "https://discord.com/api/webhooks/1149704323857657976/7jUG3nOop-1zIIs7GxKiQ_BxVyBPsrIzqX9CeUjYA29TaQYeTL_5rt78H7CMECt5UzXx";
 let disc2     = "https://spicy-ghost-94.webhook.cool";
 let txtbox    = document.getElementById("content");
@@ -8,7 +10,7 @@ async function send() {
      txtbox.innerText = "";
 
     // ==========================
-  
+  /*
     let data = { 
       username: "Git-io D2 status",
       content: "Status: TEST\n" + msg
@@ -24,6 +26,6 @@ async function send() {
     .catch(error => {
       console.error('Error:', error); 
     });
-  
+  */
 // ==============================
 } // end send
