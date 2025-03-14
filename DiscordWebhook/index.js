2a0bcf892419348f74261bca40c9b6d79dce44fa
Xiija <36306194+Xiija@users.noreply.github.com>
2025-03-13 18:00:00 -0700

    Update index.js

--- DiscordWebhook/index.js
+++ DiscordWebhook/index.js
@@ -1,9 +1,10 @@
 let disc      = "https://discord.com/api/webhooks/1149704323857657976/7jUG3nOop-1zIIs7GxKiQ_BxVyBPsrIzqX9CeUjYA29TaQYeTL_5rt78H7CMECt5UzXx";
-
+let txtbox    = document.getElementById("content");
 async function send() {  
+    let  msg = txtbox.value;
     let data = { 
       username: "Git-io D2 status",
-      content: "Status: TEST"
+      content: "Status: TEST\n" + msg
     };    
     fetch(disc, {
       method: 'POST',
@@ -11,6 +12,7 @@ async function send() {
         'Content-Type': 'application/json' 
       },
       body: JSON.stringify(data); 
+      txtbox.innerText = "";
     })
     .catch(error => {
       console.error('Error:', error); 
