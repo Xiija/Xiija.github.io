f64a80c31ad6f773e4149678110e72725a2fbe07
Xiija <36306194+Xiija@users.noreply.github.com>
2025-03-13 17:27:59 -0700

    Update index.js

--- Destiny2/index.js
+++ Destiny2/index.js
@@ -1,7 +1,7 @@
 let disc      = "https://discord.com/api/webhooks/1149704323857657976/7jUG3nOop-1zIIs7GxKiQ_BxVyBPsrIzqX9CeUjYA29TaQYeTL_5rt78H7CMECt5UzXx";
 let url       = "https://www.bungie.net/Platform/Settings";
 let dyn       = document.getElementById("dynamicPart");  
-let tog       = 0;
+let tog       = 1;
 async function refresh() {  
     fetch(url)
     .then(res  => res.json())
