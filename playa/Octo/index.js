35a3b30acddf7c13d7d40883d3625b3d310dd73d
Xiija <36306194+Xiija@users.noreply.github.com>
2025-10-07 02:34:09 -0700

    Update index.js

--- playa/Octo/index.js
+++ playa/Octo/index.js
@@ -3,7 +3,7 @@
 //
 // ==========================================================
  
-  let url3         = "https://docs.google.com/spreadsheets/d/1CktAnxWmQySR0WkbxyVF1k_JpXwCZwFDAqsWvzK1xng/gviz/tq?tqx=out:json&"; // 2025
+  let url3         = "https://docs.google.com/spreadsheets/d/1CktAnxWmQySR0WkbxyVF1k_JpXwCZwFDAqsWvzK1xng/gviz/tq?tqx=out:json&"; // 2025 octo regions
   let url2         = "https://docs.google.com/spreadsheets/d/1Q6lT1r9Dq55z3H_DQS32-Th1Ws8Lf6NhibvfEzf4fs8/gviz/tq?tqx=out:json&";
   let url          = "https://docs.google.com/spreadsheets/d/1yoALf1_MGmwyohAwnuFtg3zyjSKxyxwlPwM6VtsQ78E/gviz/tq?tqx=out:json&"; 
   let tail         = "sheet=Regions&range=B13";
