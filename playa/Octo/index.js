20b5f8d89582f5e968bef5aaef1065fee093d917
Xiija <36306194+Xiija@users.noreply.github.com>
2026-09-16 07:10:36 -0700

    Update index.js

--- playa/Octo/index.js
+++ playa/Octo/index.js
@@ -3,11 +3,9 @@
 //
 // ==========================================================
  
-  let url3         = "https://docs.google.com/spreadsheets/d/1CktAnxWmQySR0WkbxyVF1k_JpXwCZwFDAqsWvzK1xng/gviz/tq?tqx=out:json&"; // 2025 octo regions
-  let url2         = "https://docs.google.com/spreadsheets/d/1Q6lT1r9Dq55z3H_DQS32-Th1Ws8Lf6NhibvfEzf4fs8/gviz/tq?tqx=out:json&";
-  let url          = "https://docs.google.com/spreadsheets/d/1yoALf1_MGmwyohAwnuFtg3zyjSKxyxwlPwM6VtsQ78E/gviz/tq?tqx=out:json&"; 
-  let tail         = "sheet=TEST&range=A1";   // "sheet=Regions&range=B13";
- // see sheet at:     https://docs.google.com/spreadsheets/d/1yoALf1_MGmwyohAwnuFtg3zyjSKxyxwlPwM6VtsQ78E/edit#gid=0
+  let url         = "https://docs.google.com/spreadsheets/d/1CktAnxWmQySR0WkbxyVF1k_JpXwCZwFDAqsWvzK1xng/gviz/tq?tqx=out:json&"; // 2025 octo regions 
+  let tail         = "sheet=Regions&range=B13"; // "sheet=TEST&range=A1";   // 
+ // see sheet at:    https://docs.google.com/spreadsheets/d/1ZAQBA-5DYboW2OEwCC8hX2LEO1J4P7-8BXeH-c-05KU/edit?gid=0#gid=0
 // ----------------
 window.addEventListener('beforeunload', function(event) {
 //  event.preventDefault();
@@ -23,9 +21,8 @@ window.addEventListener('scroll', function() {
 });
 
 //fetch url from google and http req it.
-async function refresh() {  
-    //  region =  "sheet=Regions";
-      let gsheet = url3  + tail;
+async function refresh() {     
+      let gsheet = url  + tail;
       await fetch(gsheet)
       .then(res  => res.text())
       .then(data => {
